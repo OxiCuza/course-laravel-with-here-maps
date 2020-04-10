0a6871c2f4941bcf540b14c9117bcf621fb62359
OxiCuza <oxicusa@gmail.com>
2020-04-11 06:27:56 +0700

    add eloquent in model

--- app/Space.php
+++ app/Space.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Space extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function spacePhotos()
+    {
+        return $this->hasMany(SpacePhoto::class, 'space_id', 'id');
+    }
 }
