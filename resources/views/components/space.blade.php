400c2d7d417c2c46f28620f69c070cc24855ae5b
OxiCuza <oxicusa@gmail.com>
2020-04-13 19:54:47 +0700

    add component in view index

---
+++ resources/views/components/space.blade.php
@@ -0,0 +1,15 @@
+<div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="d-flex justify-content-between mb-3">
+            <div class="create-space">
+                <a href="#" class="btn btn-primary">
+                    PIN
+                </a>
+            </div>
+            <div id="view-space">
+                <a href="#" class="btn btn-secondary"><i class="fas fa-list"></i></a> |
+                <a href="#" class="btn btn-secondary"><i class="fas fa-globe"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
