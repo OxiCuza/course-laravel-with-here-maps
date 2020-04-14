395402fafee0d657d120ef67e8406e88d9fce748
OxiCuza <oxicusa@gmail.com>
2020-04-14 23:00:43 +0700

    add map marker in create space

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -54,8 +54,7 @@
                     <div class="form-group">
                         <label for="">Latitude</label>
                         {!! Form::text('latitude', null, ['class' => $errors->has('latitude') ? 'form-control
-                        is-invalid' :
-                        'form-control']) !!}
+                        is-invalid' : 'form-control', 'id' => 'lat']) !!}
                         @error('latitude')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{$message}}</strong>
@@ -65,8 +64,7 @@
                     <div class="form-group">
                         <label for="">Longitude</label>
                         {!! Form::text('longitude', null, ['class' => $errors->has('longitude') ? 'form-control
-                        is-invalid' :
-                        'form-control']) !!}
+                        is-invalid' : 'form-control', 'id' => 'lng']) !!}
                         @error('longitude')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{$message}}</strong>
@@ -81,3 +79,9 @@
     </div>
 </div>
 @endsection
+
+@push('script')
+    <script>
+        window.action = "submit"
+    </script>
+@endpush
